1719f693c38d65804e831839f418ea946d3272bd
DanielReborn <danielreborn2019@gmail.com>
2023-06-09 05:43:05 +0100

    alx mandatory tasks

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+File_name: 0-add.py
+Created: 9th of June, 2023
+Auth: Oni Ajibola Taiwo (Daniel Reborn)
+Size: undefined
+Project: 0x02-python-import_modules
+Status: submitted.
+"""
+if __name__ == '__main__':
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+File_name: 5-variable_load.py
+Created: 9th of June, 2023
+Auth: Oni Ajibola Taiwo (Daniel Reborn)
+Size: undefined
+Project: 0x02-python-import_modules
+Status: submitted.
+"""
+
+if __name__ == '__main__':
+    from variable_load_5 import a
+    print(a)
